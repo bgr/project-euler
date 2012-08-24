e14d6a1e50492aaedc30eab53a9f5be3948d27ae
bgr <bgr.gyk@gmail.com>
2012-08-24 20:46:03 +0200

    Solutions to 19 and 20

---
+++ p019.py
@@ -0,0 +1,16 @@
+﻿# How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+days_in_months = [31,None,31,30,31,30,31,31,30,31,30,31]
+
+day_in_week = 0
+sundays = 0
+for year in range(1900, 2001):
+  is_leap = (year%100!=0 and year%4==0) or (year%100==0 and year%400==0)
+  for m in range(12):
+    days_in_this_month = days_in_months[m] if m!=1 else 29 if is_leap else 28
+    for date in range(days_in_this_month):
+      if date==0 and day_in_week==6 and year>1900:
+        sundays += 1
+      day_in_week = (day_in_week+1) % 7
+      
+print sundays
---
+++ p020.py
@@ -0,0 +1,29 @@
+﻿# Find the sum of the digits in the number 100!
+
+# easy solution using bigint support
+fact = reduce(lambda a,b: a*b, range(1,101))
+print reduce(lambda a,b: int(a)+int(b), str(fact))
+
+# hard solution using string as bigint
+def bigAdd(n1, n2):
+  if len(n1) < len(n2):
+    n1 = n1.zfill(len(n2))
+  elif len(n2) < len(n1):
+    n2 = n2.zfill(len(n1))
+  result = ''
+  carry = 0
+  for d in reversed(range(len(n1))):
+    t = int(n1[d]) + int(n2[d]) + carry
+    carry = 1 if t > 9 else 0
+    result = str(t % 10) + result
+  if carry != 0:
+    result = '1' + result
+  return result
+  
+fact = '1'
+for i in range(2,101):
+  temp_fact = fact
+  for _ in range(i-1):
+    fact = bigAdd(fact,temp_fact)
+  
+print reduce(lambda a,b: int(a)+int(b), str(fact))
