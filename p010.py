eaf07412b935a8b309f4ffb26560c17904286aa0
bgr <bgr.gyk@gmail.com>
2012-08-24 17:53:17 +0200

    Updated prime sieve in 10

--- p010.py
+++ p010.py
@@ -1,17 +1,15 @@
 # Calculate the sum of all the primes below two million.
 
-def genPrimes(limit):
+from math import sqrt
+
+def getPrimes(limit):
   sieveTable = [True for n in range(2,limit)]
-  for i,stillValid in enumerate(sieveTable):
-    if stillValid:
-      jumpStep = i+2 # numbers are offset by 2 
-      j = i+jumpStep
-      while j<len(sieveTable):
+  for i in range(2, int(sqrt(limit))+1):
+    if sieveTable[i-2]: # numbers are offset by 2
+      for j in range(i*i-2, limit-2, i):
         sieveTable[j] = False
-        j+=jumpStep
-      yield i+2
+  return [i+2 for i,v in enumerate(sieveTable) if v]
 
 
-primes = [i for i in genPrimes(2000000)]
-
+primes = getPrimes(2000000)
 print sum(primes)
