30c04b05b18ea1222835cc578d1eaa7f751ae619
bgr <bgr.gyk@gmail.com>
2012-08-25 17:59:20 +0200

    Solutions to 22 and 23

---
+++ p022.py
@@ -0,0 +1,17 @@
+﻿# What is the total of all the name scores in the file?
+# begin by sorting it into alphabetical order. Then working out the 
+# alphabetical value for each name, multiply this value by its alphabetical 
+# position in the list to obtain a name score.
+# For example, when the list is sorted into alphabetical order, 
+# COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+# So, COLIN would obtain a score of 938 * 53 = 49714.
+
+data = open('p022_input.txt', 'r').read()
+names = sorted([n[1:-1] for n in data.split(',')])
+
+def get_alphabetical_value(str):
+  from string import ascii_lowercase
+  return sum([ascii_lowercase.index(i)+1 for i in str.lower()])
+
+
+print sum([get_alphabetical_value(v)*(i+1) for i,v in enumerate(names)])
---
+++ p023.py
@@ -0,0 +1,26 @@
+﻿# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+def divisors(n):
+  from math import sqrt
+  divs = [1]
+  for i in xrange(2, int(sqrt(n))+1):
+    if n % i == 0 and n != i:
+      divs.append(i)
+      if i < sqrt(n):
+        divs.append(n/i)
+  return sorted(divs)
+
+
+abundant_set = {i for i in xrange(1,28124-12) if sum(divisors(i)) > i}
+
+def is_sum_of_abundants(n):
+  for i in abundant_set:
+    if n-i in abundant_set:
+      return True
+    if i>n:
+      return False
+  print 'gone over'
+  return False
+
+result = [i for i in xrange(1,28124) if not is_sum_of_abundants(i)]
+print sum(result)
