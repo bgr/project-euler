27faaefc9e5bc23ce11e7a1e5ac7d0effec00ff9
bgr <bgr.gyk@gmail.com>
2012-08-26 21:33:17 +0200

    Solutions to 29, 30 and 31

---
+++ p030.py
@@ -0,0 +1,24 @@
+﻿# Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+def is_sum_of_digits(num, power):
+  if num == 0 or num == 1: # exceptions by definition
+    return False
+  digits_powered = map(lambda a: int(a)**power, str(num))
+  return num == sum(digits_powered)
+  
+assert is_sum_of_digits(1634,4)
+assert is_sum_of_digits(8208,4)
+assert is_sum_of_digits(9474,4)
+assert not is_sum_of_digits(1635,4)
+assert not is_sum_of_digits(8207,4)
+assert not is_sum_of_digits(9473,4)
+assert not is_sum_of_digits(2,4)
+assert not is_sum_of_digits(1,4)
+assert not is_sum_of_digits(0,4)
+
+# limit is the first value where largest i-digit number is larger
+# than maximum sum that can be obtained with powered digits
+limit = [i*(9**5) for i in range(10) if 10**i-1 > i*9**5][0]
+numbers = [i for i in range(limit) if is_sum_of_digits(i,5)]
+print 'numbers:', numbers
+print 'sum:', sum(numbers)
---
+++ p031.py
@@ -0,0 +1,17 @@
+﻿# How many different ways can £2 be made using any number of coins?
+# Available coins: 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+
+def combinations(target, values):
+  # values = sorted(values)
+  n = 0
+  for i,val in enumerate(values):
+    for times in range(1,target/val+1):
+      if val*times > target:
+        continue
+      if val*times == target:
+        n += 1
+      n += combinations(target-val*times, values[i+1:])
+  return n
+
+  
+print combinations(200, [1,2,5,10,20,50,100,200])
